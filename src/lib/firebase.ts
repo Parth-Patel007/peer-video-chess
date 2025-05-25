6a0d1981a0e0e070f65d01b190bc682e59bb75fb
Parth-Patel007 <ppate220@asu.edu>
2025-05-24 21:50:11 -0700

    sec issue solvred

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,6 +6,7 @@ import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
 import { useState, useEffect } from 'react';
 
 const firebaseConfig = {
+  
   apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
